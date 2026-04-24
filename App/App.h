c4aa55ded760448c8b72b4075e5813ebff9a5a66
ShaF <kdt24sgc40005@edu.kobe-tech.ac.jp>
2026-04-24 17:32:41 +0900

    プロジェクト ファイルを追加します。

---
+++ App/App.h
@@ -0,0 +1,32 @@
+﻿#pragma once
+#include <Windows.h>
+#include <cstdint>
+
+
+class App
+{
+public:
+
+    App(uint32_t width, uint32_t height);
+    ~App();
+    void Run();
+
+private:
+
+    // インスタンスハンドル
+    HINSTANCE   m_hInst;
+    // ウィンドウハンドル
+    HWND        m_hWnd;
+    // ウィンドウの横幅
+    uint32_t    m_Width;
+    // ウィンドウの縦幅
+    uint32_t    m_Height;
+
+    bool InitApp();
+    void TermApp();
+    bool InitWnd();
+    void TermWnd();
+    void MainLoop();
+
+    static LRESULT CALLBACK WndProc(HWND hWnd, UINT msg, WPARAM wp, LPARAM lp);
+};
---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include "App/App.h"
+
+int wmain(int argc, wchar_t** argv, wchar_t** evnp)
+{
+    App app(960, 540);
+    app.Run();
+
+    return 0;
+}
